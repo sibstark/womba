0153f63f7badb8e6815bc5ff62281871908b9610
Mamyan Manuk <manuk20061984@yandex.ru>
2023-05-28 10:43:24 +0300

    feat: added 404

--- packages/client/src/containers/Router/index.tsx
+++ packages/client/src/containers/Router/index.tsx
@@ -7,13 +7,15 @@ import Home from '../Home'
 import Login from '../Login'
 import Forum from '../Forum'
 import Layout from '../Layout'
+import Error from '../Error'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forum" element={<Forum />} />
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="forum" element={<Forum />} />
+      <Route path="*" element={<Error />} />
     </Route>
   )
 )
