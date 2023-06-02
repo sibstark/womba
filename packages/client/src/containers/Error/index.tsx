0f85b68056565c9806d8fd88a00d4431c4e262d5
Aleksandr Komarov <komar81@gmail.com>
2023-06-02 19:11:13 +0200

    Добавил ErrorBoundry в проект

--- packages/client/src/containers/Error/index.tsx
+++ packages/client/src/containers/Error/index.tsx
@@ -1,5 +1,48 @@
-const Error = () => {
-  return <div> 404 </div>
+import React, { ErrorInfo, ReactNode } from 'react'
+import Layout from '../../ui/components/Layout'
+import Error from '../../ui/components/Error'
+
+type ErrorBoundaryProps = {
+  children: ReactNode
 }
 
-export default Error
+type ErrorBoundaryState = {
+  hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = {
+      hasError: false,
+    }
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log(error, errorInfo)
+  }
+
+  render() {
+    const { hasError } = this.state
+    const { children } = this.props
+
+    if (hasError) {
+      return (
+        <Layout>
+          <Error />
+        </Layout>
+      )
+    }
+
+    return children
+  }
+}
+
+export default ErrorBoundary
