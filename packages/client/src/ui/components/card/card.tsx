f8428eeaac6b740dff1af7ff3a2a070df3f7b283
Oleg <oleg.bryuhanov@hotmail.com>
2023-06-02 13:48:53 +0400

    Login page and login form

---
+++ packages/client/src/ui/components/card/card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { classnames } from '@utils'
+import './styles.scss'
+
+type CardProps = React.HTMLAttributes<HTMLDivElement>
+export const Card: React.FC<CardProps> = ({ className, ...rest }) => {
+  const classes = classnames(className, 'card')
+  return <div className={classes} {...rest} />
+}
