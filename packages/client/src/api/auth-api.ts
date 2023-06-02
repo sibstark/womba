20cea63ecd51af0e11eeca9e6b8c8d3415cbab60
Oleg <oleg.bryuhanov@hotmail.com>
2023-06-02 16:27:54 +0400

    controllers and api

---
+++ packages/client/src/api/auth-api.ts
@@ -0,0 +1,36 @@
+import { SigninRequest, User, SignupRequest } from '@types'
+import BaseAPI from './base-api'
+
+export class AuthAPI extends BaseAPI {
+  constructor() {
+    super('/auth')
+  }
+
+  signup(data: SignupRequest) {
+    return this.http.post('/signup', {
+      data,
+    })
+  }
+
+  singin(data: SigninRequest) {
+    return this.http.post('/signin', {
+      data,
+    })
+  }
+
+  logout() {
+    return this.http.post('/logout')
+  }
+
+  getUser() {
+    return this.http.get<User>('/user')
+  }
+
+  create = undefined
+
+  update = undefined
+
+  delete = undefined
+
+  read = undefined
+}
