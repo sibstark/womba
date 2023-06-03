0eadaa18151cdb8b971e962ca91de654340f980a
Oleg <oleg.bryuhanov@hotmail.com>
2023-06-03 19:04:30 +0400

    form control template

--- packages/client/src/containers/registration/index.tsx
+++ packages/client/src/containers/registration/index.tsx
@@ -1,7 +1,6 @@
-import { Button, FormControl, FormError, Input, Label } from '@ui/components'
-import { TChildrenArguments, withForm } from '../form'
+import { Button } from '@ui/components'
+import { TChildrenArguments, withForm, FormControlInputTemplate } from '../form'
 import React from 'react'
-import classnames from 'classnames'
 import {
   emailValidation,
   loginValidation,
@@ -28,134 +27,91 @@ const onSubmit = (
   console.log(values)
 }
 
-const RenderRegistrationForm: React.FC<RenderRegistrationFormProps> = ({
-  register,
-  formState,
-  watch,
-}) => {
+const RenderRegistrationForm: React.FC<RenderRegistrationFormProps> = props => {
+  const { formState, watch } = props
   const { errors } = formState
   return (
     <>
-      <FormControl>
-        <Label>Имя</Label>
-        <Input
-          placeholder="Имя"
-          {...register('name', {
-            required: 'Обязательно',
-            validate: nameValidation,
-          })}
-          className={classnames({ 'input--error': errors.name })}
-        />
-        {errors.name && (
-          <FormError className="form-error--shown">
-            {errors.name.message}
-          </FormError>
-        )}
-      </FormControl>
-      <FormControl>
-        <Label>Фамилия</Label>
-        <Input
-          placeholder="Фамилия"
-          {...register('surname', {
-            required: 'Обязательно',
-            validate: nameValidation,
-          })}
-          className={classnames({
-            'input--error': errors.surname,
-          })}
-        />
-        {errors.surname && (
-          <FormError className="form-error--shown">
-            {errors.surname.message}
-          </FormError>
-        )}
-      </FormControl>
-      <FormControl>
-        <Label>Email</Label>
-        <Input
-          placeholder="Email"
-          {...register('email', {
-            required: 'Обязательно',
-            validate: emailValidation,
-          })}
-          className={classnames({ 'input--error': errors.email })}
-        />
-        {errors.email && (
-          <FormError className="form-error--shown">
-            {errors.email.message}
-          </FormError>
-        )}
-      </FormControl>
-      <FormControl>
-        <Label>Телефон</Label>
-        <Input
-          placeholder="Телефон"
-          {...register('phone', {
-            required: 'Обязательно',
-            validate: phoneValidation,
-          })}
-          className={classnames({ 'input--error': errors.phone })}
-        />
-        {errors.phone && (
-          <FormError className="form-error--shown">
-            {errors.phone.message}
-          </FormError>
-        )}
-      </FormControl>
-      <FormControl>
-        <Label>Логин</Label>
-        <Input
-          placeholder="Логин"
-          {...register('login', {
-            required: 'Обязательно',
-            validate: loginValidation,
-          })}
-          className={classnames({ 'input--error': errors.login })}
-        />
-        {errors.login && (
-          <FormError className="form-error--shown">
-            {errors.login.message}
-          </FormError>
-        )}
-      </FormControl>
-      <FormControl>
-        <Label>Пароль</Label>
-        <Input
-          type="password"
-          placeholder="Пароль"
-          {...register('password', {
-            required: 'Обязательно',
-            validate: passValidation,
-          })}
-          className={classnames({ 'input--error': errors.password })}
-        />
-        {errors.password && (
-          <FormError className="form-error--shown">
-            {errors.password.message}
-          </FormError>
-        )}
-      </FormControl>
-      <FormControl>
-        <Input
-          type="password"
-          placeholder="Пороль еще раз"
-          {...register('confirmPassword', {
-            required: 'Обязательно',
-            validate: (val: string) => {
-              if (watch('password') !== val) {
-                return 'Пароли не совпадают'
-              }
-              return true
-            },
-          })}
-          className={classnames({ 'input--error': errors.confirmPassword })}
-        />
-        {errors.confirmPassword && (
-          <FormError className="form-error--shown">
-            {errors.confirmPassword.message}
-          </FormError>
-        )}
-      </FormControl>
+      <FormControlInputTemplate<TRegistrationForm>
+        {...props}
+        placeholder="Имя"
+        name="name"
+        title="Имя"
+        options={{
+          required: 'Обязательно',
+          validate: nameValidation,
+        }}
+      />
+      <FormControlInputTemplate<TRegistrationForm>
+        {...props}
+        placeholder="Фамилия"
+        name="surname"
+        title="Фамилия"
+        options={{
+          required: 'Обязательно',
+          validate: nameValidation,
+        }}
+      />
+      <FormControlInputTemplate<TRegistrationForm>
+        {...props}
+        placeholder="Email"
+        name="email"
+        title="Email"
+        options={{
+          required: 'Обязательно',
+          validate: emailValidation,
+        }}
+      />
+      <FormControlInputTemplate<TRegistrationForm>
+        {...props}
+        placeholder="Телефон"
+        name="phone"
+        title="Телефон"
+        options={{
+          required: 'Обязательно',
+          validate: phoneValidation,
+        }}
+      />
+      <FormControlInputTemplate<TRegistrationForm>
+        {...props}
+        placeholder="Логин"
+        name="login"
+        title="Логин"
+        options={{
+          required: 'Обязательно',
+          validate: loginValidation,
+        }}
+      />
+      <FormControlInputTemplate<TRegistrationForm>
+        {...props}
+        placeholder="Пароль"
+        name="password"
+        title="Пароль"
+        options={{
+          required: 'Обязательно',
+          validate: passValidation,
+        }}
+        inputProps={{
+          type: 'password',
+        }}
+      />
+      <FormControlInputTemplate<TRegistrationForm>
+        {...props}
+        placeholder="Пороль еще раз"
+        name="confirmPassword"
+        options={{
+          required: 'Обязательно',
+          validate: (val: string) => {
+            if (watch('password') !== val) {
+              return 'Пароли не совпадают'
+            }
+            return true
+          },
+        }}
+        inputProps={{
+          type: 'password',
+        }}
+      />
       <Button type="submit" className="button--blue w-100">
         Зарегистрироваться
       </Button>
