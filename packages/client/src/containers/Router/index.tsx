8a1a513c35ea2fcb7dc6dc32cdac73424a961a04
iDenisOff <andreyfrolovdda@gmail.com>
2023-06-05 18:12:56 +0300

    Страница форума

--- packages/client/src/containers/Router/index.tsx
+++ packages/client/src/containers/Router/index.tsx
@@ -4,18 +4,19 @@ import {
   Route,
 } from 'react-router-dom'
 import Home from '../Home'
-import Forum from '../Forum'
 import Layout from '../Layout'
 import Error from '../Error'
 import { Routes } from './routes'
 import { RegistrationPage } from '@pages/registration'
 import { LoginPage } from '@pages/login'
+import { ForumPage } from '@pages/forum'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path={Routes.Forum} element={<Forum />} />
+      <Route path={Routes.Login} element={<LoginPage />} />
+      <Route path={Routes.Forum} element={<ForumPage />} />
       <Route path={Routes.Registration} element={<RegistrationPage />} />
       <Route path={Routes.Login} element={<LoginPage />} />
       <Route path="*" element={<Error />} />
