f8b0b9c4bba958573e6cf44176267854e1f6ac95
iDenisOff <andreyfrolovdda@gmail.com>
2023-06-07 17:31:13 +0300

    Правки

--- packages/client/src/pages/Game/components/Footer/index.tsx
+++ packages/client/src/pages/Game/components/Footer/index.tsx
@@ -5,9 +5,9 @@ import './styles.scss';
 export const Footer: React.FC = () => {
   return (
     <div className="footer">
-      <span className="footer_title">Как играть</span>
+      <span className="footer__title">Как играть</span>
       <br/>
-      <span className="footer_text">
+      <span className="footer__text">
         Используйте стрелки, чтобы перемещать плитки по полю.
         Когда две плитки с одинаковым номиналом соприкасаются, они сливаются в одну!
       </span>
