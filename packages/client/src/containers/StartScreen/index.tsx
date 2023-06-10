be6fd27ae36db23d3870992f0e92ac35980acc0b
p.bychkov <wononi02144@gmail.com>
2023-06-10 15:22:39 +0300

    Переписаны названия классов и убирает лишний тег

--- packages/client/src/containers/StartScreen/index.tsx
+++ packages/client/src/containers/StartScreen/index.tsx
@@ -2,19 +2,17 @@ import { Button } from '@ui/components'
 
 export const StartScreen = () => {
   return (
-    <>
-      <div className="start__wrapper">
-        <div className="logo__wrapper">
+      <div className="start__screen">
+        <div className="start__screen-logo__wrapper">
           <h2>WOMBA</h2>
-          <div className="logo">
+          <div className="start__screen-logo__wrapper-logo">
             <p>2</p>
             <p>0</p>
             <p>4</p>
             <p>8</p>
           </div>
         </div>
-        <Button className={'start__btn'}>Начать играть</Button>
+        <Button className={'start__screen-btn'}>Начать играть</Button>
       </div>
-    </>
   )
 }
