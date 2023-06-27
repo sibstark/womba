1ca46ee67a8a69a5c2effb2eef1ae1d8cea1982f
Wononi <wononi02144@gmail.com>
2023-06-27 14:02:19 +0300

    Исправление ошибок

--- packages/client/src/containers/EndScreen/index.tsx
+++ packages/client/src/containers/EndScreen/index.tsx
@@ -3,11 +3,21 @@ import { NavLink } from 'react-router-dom'
 import { Typography } from '../../ui/components/Typography'
 
 export const EndScreen = () => {
-  return <div className='end__wrapper'>
-    <Typography variant='h2' component='h2'>Конец игры</Typography>
-    <Typography variant='p' component='p'>Ваш счет:</Typography>
-    <Typography variant='p' component='p'>2048</Typography>
-    <Button className='end__btn'>Заново</Button>
-    <NavLink className='end__btn' to={'/'}>На главную</NavLink>
-  </div>
+  return (
+    <div className="end__wrapper">
+      <Typography variant="h2" component="h2">
+        Конец игры
+      </Typography>
+      <Typography variant="p" component="p">
+        Ваш счет:
+      </Typography>
+      <Typography variant="p" component="p">
+        2048
+      </Typography>
+      <Button className="end__btn">Заново</Button>
+      <NavLink className="end__btn" to="/">
+        На главную
+      </NavLink>
+    </div>
+  )
 }
