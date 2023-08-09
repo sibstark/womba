ac7dd8bfca962f50be64c4d3474f98eeb57a056a
Aleksandr Komarov <komar81@gmail.com>
2023-08-09 19:49:43 +0200

    env to compese

--- packages/server/services/SequelizeClient.ts
+++ packages/server/services/SequelizeClient.ts
@@ -1,17 +1,17 @@
 import { Sequelize, SequelizeOptions } from "sequelize-typescript";
 
-import { dbUser, dbPassword, dbName, dbHost } from "../env";
+import { dbUser, dbPassword, dbName } from "../env";
 import Comment from "../models/Comment";
 import Post from "../models/Post";
 import Reaction from "../models/Reaction";
 import Reply from "../models/Reply";
 import Theme from "../models/Theme";
 import User, { userBeforeCreate } from "../models/User";
-
+//
 const sequelizeOptions: SequelizeOptions = {
     database: dbName,
     dialect: "postgres",
-    host: dbHost,
+    host: "postgres",
     logging: true,
     models: [User, Comment, Post, Reaction, Reply, Theme],
     password: dbPassword,
